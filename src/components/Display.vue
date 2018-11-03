3888659dca41f9d37c31dd703ae796cd09fc819b
Administrator <garrlker@hotmail.com>
2018-11-02 22:14:32 -0500

    Added a couple roms to a file for later use, Chip8 display component is functional. Time to start on the Chip8 internals itself

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
-    <div class="ui-menu">
-      <!-- <select v-model="zoom">
-      <option value=12>Big</option>
-      <option value=6>Small</option>
-    </select> -->
-    </div>
     <div class="ui-child">
-      <canvas id="myCanvas" style="border:1px solid #000000;">
+      <canvas id="emuCanvas" :width="dispWidth" :height="dispHeight" style="border:1px solid #000000;">
       </canvas>
+      <button @click="updateDisplay">Update</button>
+      <input v-model="tempX" type="number">
+      <input v-model="tempY" type="number">
+      <input v-model="tempVal" type="number">
+      <button @click="putPixel">Put</button>
     </div>
   </div>
 </template>
@@ -21,30 +20,86 @@ export default {
   },
   data: function() {
     return {
+      canvas: null,
+      ctx: null,
       dispWidth: 64,
       dispHeight: 32,
       dispBuff: null,
-      zoom: 12
+      canvasLen: 8192,
+      zoom: 12,
+      tempX: 0,
+      tempY: 0,
+      tempVal: 0
     };
   },
   computed: {
-    zoomWidth: function() {
-      return this.dispWidth * this.zoom;
+    canvasWidth: function() {
+      return this.canvas.width;
     },
-    zoomHeight: function() {
-      return this.dispHeight * this.zoom;
+    canvasHeight: function() {
+      return this.canvas.height;
+    }
+  },
+  methods: {
+    putPixel: function() {
+      // This is for debug purposes only
+      // Eventually tempX,Y,and Val will be erased
+      // The UI will be too
+      this.dispBuff[
+        64 * parseInt(this.tempY) + parseInt(this.tempX)
+      ] = parseInt(this.tempVal);
+      console.log("Pixel: ", 64 * this.tempY + this.tempX);
+      console.log("Disp Buff: ", this.dispBuff);
+      this.updateDisplay();
+    },
+    updateDisplay: function() {
+      // Grab the current canvas data object
+      var imageData = this.ctx.getImageData(
+        0,
+        0,
+        this.canvas.width,
+        this.canvas.height
+      );
+
+      // Grab the buffer
+      var data = imageData.data;
+
+      // Copy chip8 buffer into canvas buffer
+      for (var i = 0; i < this.canvasLen; i += 4) {
+        if (this.dispBuff[i / 4]) {
+          data[i] = 255;
+          data[i + 1] = 255;
+          data[i + 2] = 255;
+          data[i + 3] = 255;
+        } else {
+          data[i] = 0;
+          data[i + 1] = 0;
+          data[i + 2] = 0;
+          data[i + 3] = 255;
+        }
+      }
+
+      // Put it back ┬─┬ ノ( ゜-゜ノ)
+      this.ctx.putImageData(imageData, 0, 0);
     }
   },
   mounted: function() {
+    // Grab reference to HTML Canvas
+    this.canvas = document.getElementById("emuCanvas");
+    this.ctx = this.canvas.getContext("2d");
+
     // Set the array length and fill it with 0s
-    this.dispBuff = Array(this.dispWidth * this.dispHeight).fill(1);
+    this.dispBuff = Array(this.dispWidth * this.dispHeight).fill(0);
+
+    // Update the display
+    this.updateDisplay();
   }
 };
 </script>
 
 <style scoped>
 canvas {
-  width: 100%;
-  height: 100%;
+  min-width: 100%;
+  min-height: 100%;
 }
 </style>
